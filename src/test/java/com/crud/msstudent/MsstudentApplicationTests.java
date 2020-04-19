602dbc9faa64ccb26da638b4afc857363ab2eb65
azhwani <azhwani@devwithus.com>
2020-04-19 17:29:22 +0100

    initial commit

---
+++ src/test/java/com/crud/msstudent/MsstudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.msstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsstudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
